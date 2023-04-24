ca1c956f21e920626f3752e2f045d6a99187828c
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-24 22:27:33 +0530

    updated the method

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -43,4 +43,22 @@ class FirestoreMethods {
     }
     return res;
   }
+
+  Future<void> likePost(String postId, String uid, List likes) async {
+    try {
+      if (likes.contains(uid)) {
+        await _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayRemove([uid]),
+        });
+      } else {
+        await _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayUnion([uid]),
+        });
+      }
+    } catch (e) {
+      print(
+        e.toString(),
+      );
+    }
+  }
 }
