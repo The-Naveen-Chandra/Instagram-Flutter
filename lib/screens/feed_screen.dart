d9e2d85ee2f8942dccf3cf7d9a8aa6cd14c827dd
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-24 12:19:57 +0530

    added icons and styling

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/widgets.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          // ignore: deprecated_member_use
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon:const Icon(
+              Icons.favorite_border,
+              size: 26,
+            ),
+            padding: EdgeInsets.zero,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/icons/facebook-messenger.svg',
+              // ignore: deprecated_member_use
+              color: primaryColor,
+              height: 24,
+            ),
+          ),
+        ],
+      ),
+      body: const PostCard(),
     );
   }
 }
