a8a8d424b6d9dc1aa19ccddf8d7e587bedd091eb
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-24 23:13:14 +0530

    working on comment card

---
+++ lib/widgets/comment_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CommentCard extends StatefulWidget {
+  const CommentCard({super.key});
+
+  @override
+  State<CommentCard> createState() => _CommentCardState();
+}
+
+class _CommentCardState extends State<CommentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 18,
+        horizontal: 16,
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://images.unsplash.com/photo-1682310934381-ea1437927bd0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+            ),
+            radius: 18,
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
