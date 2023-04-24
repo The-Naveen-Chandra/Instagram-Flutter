7c4509269a7d7e3aa1c62a633da1d4c2108188d3
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-24 11:42:05 +0530

    working on feed screen

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
