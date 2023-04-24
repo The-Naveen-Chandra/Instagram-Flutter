19223d3c73ee27c26e92257ced7480dff415b623
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-24 12:20:37 +0530

    working on post card

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+    );
+  }
+}
