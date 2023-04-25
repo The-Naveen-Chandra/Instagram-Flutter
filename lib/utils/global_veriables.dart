eb00cb84709bf4395ee372d1d8bdce79c60daac2
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-25 16:28:22 +0530

    added search screen

--- lib/utils/global_veriables.dart
+++ lib/utils/global_veriables.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/screens.dart';
+import 'package:instagram_clone/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('notif'),
   Text('profile'),
