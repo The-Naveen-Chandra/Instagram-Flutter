8abe1d25bbc6f4a73e2b32a65bfc7f2e414dc256
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-26 11:57:35 +0530

    exports

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,3 +2,5 @@ export './add_post_screen.dart';
 export './login_screen.dart';
 export './signup_screen.dart';
 export './feed_screen.dart';
+export './search_screen.dart';
+export './profile_screen.dart';
