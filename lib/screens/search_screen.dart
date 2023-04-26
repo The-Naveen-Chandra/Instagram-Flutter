4395e17078daf00184cbb4f87663c304af308d4a
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-26 11:59:42 +0530

    styling

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
@@ -98,7 +99,29 @@ class _SearchScreenState extends State<SearchScreen> {
                 );
               },
             )
-          : const Text('Posts'),
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 3,
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) => Image.network(
+                    (snapshot.data! as dynamic).docs[index]['postUrl'],
+                  ),
+                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                    (index % 7 == 0) ? 2 : 1,
+                    (index % 7 == 0) ? 2 : 1,
+                  ),
+                  mainAxisSpacing: 2,
+                  crossAxisSpacing: 2,
+                );
+              },
+            ),
     );
   }
 }
