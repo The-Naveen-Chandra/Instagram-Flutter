24ce3a7c1b180e7140861a31e2db55b7f8981f16
Naveen Chandra <the.naveenchandra@gmail.com>
2023-05-04 13:21:07 +0530

    fix some bugs

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/theme.dart';
@@ -18,6 +19,11 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   var userData = {};
+  int postLen = 0;
+  int followers = 0;
+  int following = 0;
+  bool isFollowing = false;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -26,6 +32,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   getData() async {
+    setState(() {
+      isLoading = true;
+    });
     try {
       var userSnap = await FirebaseFirestore.instance
           .collection('users')
@@ -33,9 +42,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
           .get();
 
       // get post length
-      var postSnap = await FirebaseFirestore.instance.collection('posts');
+      var postSnap = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get();
 
+      postLen = postSnap.docs.length;
       userData = userSnap.data()!;
+      followers = userSnap.data()!['followers'].length;
+      following = userSnap.data()!['following'].length;
+      isFollowing = userSnap
+          .data()!['followers']
+          .contains(FirebaseAuth.instance.currentUser!.uid);
+
       setState(() {});
     } catch (e) {
       showSnackBar(
@@ -43,184 +62,206 @@ class _ProfileScreenState extends State<ProfileScreen> {
         context,
       );
     }
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: Row(
-          children: [
-            const Icon(Icons.lock_outline_rounded),
-            const SizedBox(
-              width: 8,
-            ),
-            Text(
-              userData['username'],
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-              ),
-            ),
-            const SizedBox(
-              width: 8,
-            ),
-          ],
-        ),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              "assets/icons/new-post.svg",
-              // ignore: deprecated_member_use
-              color: primaryColor,
-              height: 24,
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              "assets/icons/menu-1.svg",
-              // ignore: deprecated_member_use
-              color: primaryColor,
-              height: 24,
-            ),
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
           )
-        ],
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 25,
-              top: 16,
-              bottom: 16,
-              right: 16,
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              title: Row(
+                children: [
+                  const Icon(Icons.lock_outline_rounded),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    userData['username'],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                ],
+              ),
+              centerTitle: false,
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset(
+                    "assets/icons/new-post.svg",
+                    // ignore: deprecated_member_use
+                    color: primaryColor,
+                    height: 24,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset(
+                    "assets/icons/menu-1.svg",
+                    // ignore: deprecated_member_use
+                    color: primaryColor,
+                    height: 24,
+                  ),
+                )
+              ],
             ),
-            child: Column(
+            body: ListView(
               children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      backgroundImage: NetworkImage(
-                        userData['photoUrl'],
-                      ),
-                      radius: 40,
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 25,
+                    top: 16,
+                    bottom: 16,
+                    right: 16,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
                         children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            backgroundImage: NetworkImage(
+                              userData['photoUrl'],
+                            ),
+                            radius: 40,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    buildStatColumn(postLen, "Posts"),
+                                    buildStatColumn(followers, "Followers"),
+                                    buildStatColumn(following, "Following"),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Text(
+                          userData['username'],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.only(top: 1),
+                        child: Text(
+                          userData['bio'],
+                          style: const TextStyle(
+                            height: 1.3,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          FirebaseAuth.instance.currentUser!.uid == widget.uid
+                              ? FollowButton(
+                                  text: "Edit profile",
+                                  backgroundColor: Colors.grey.shade900,
+                                  textColor: primaryColor,
+                                  function: () {},
+                                )
+                              : isFollowing
+                                  ? FollowButton(
+                                      text: "Unfollow",
+                                      backgroundColor: Colors.grey.shade900,
+                                      textColor: primaryColor,
+                                      function: () {},
+                                    )
+                                  : FollowButton(
+                                      text: "Follow",
+                                      backgroundColor: Colors.blue,
+                                      textColor: primaryColor,
+                                      function: () {},
+                                    ),
+                          const SizedBox(
+                            width: 6,
+                          ),
+                          FollowButton(
+                            text: "Share profile",
+                            backgroundColor: Colors.grey.shade900,
+                            textColor: primaryColor,
+                            function: () {},
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade900,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              height: 40,
+                              width: 40,
+                              alignment: Alignment.center,
+                              child: const Icon(
+                                Icons.person_add,
+                                size: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 20),
                           Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              buildStatColumn(20, "Posts"),
-                              buildStatColumn(150, "Followers"),
-                              buildStatColumn(10, "Following"),
+                            children: const [
+                              HighlightCircle(
+                                text: "Cars",
+                                photoUrl:
+                                    "https://images.unsplash.com/photo-1682329782353-1f06c794b5b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+                              ),
+                              HighlightCircle(
+                                text: "💖",
+                                photoUrl:
+                                    "https://images.unsplash.com/photo-1682356828551-96826d14382b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+                              ),
+                              HighlightCircle(
+                                text: "New",
+                                isadd: true,
+                              ),
                             ],
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(top: 10),
-                  child:  Text(
-                    userData['username'],
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+                    ],
                   ),
-                ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(top: 1),
-                  child:  Text(
-                    userData['bio'],
-                    style: const TextStyle(
-                      height: 1.3,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    FollowButton(
-                      text: "Edit profile",
-                      backgroundColor: Colors.grey.shade900,
-                      textColor: primaryColor,
-                      function: () {},
-                    ),
-                    const SizedBox(
-                      width: 6,
-                    ),
-                    FollowButton(
-                      text: "Share profile",
-                      backgroundColor: Colors.grey.shade900,
-                      textColor: primaryColor,
-                      function: () {},
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade900,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        height: 40,
-                        width: 40,
-                        alignment: Alignment.center,
-                        child: const Icon(
-                          Icons.person_add,
-                          size: 20,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 20),
-                    Row(
-                      children: const [
-                        HighlightCircle(
-                          text: "Cars",
-                          photoUrl:
-                              "https://images.unsplash.com/photo-1682329782353-1f06c794b5b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-                        ),
-                        HighlightCircle(
-                          text: "💖",
-                          photoUrl:
-                              "https://images.unsplash.com/photo-1682356828551-96826d14382b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-                        ),
-                        HighlightCircle(
-                          text: "New",
-                          isadd: true,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                )
               ],
             ),
-          )
-        ],
-      ),
-    );
+          );
   }
 
   Column buildStatColumn(int num, String label) {
