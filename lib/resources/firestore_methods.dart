cf443f7290327534e6833ddb4928c85c808ed31a
Naveen Chandra <the.naveenchandra@gmail.com>
2023-05-08 17:16:26 +0530

    follow user method

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -107,4 +107,36 @@ class FirestoreMethods {
     }
     return res;
   }
+
+  // follow user
+  Future<void> followUser(
+    String uid,
+    String followId,
+  ) async {
+    try {
+      DocumentSnapshot snap =
+          await _firestore.collection('users').doc(uid).get();
+      List following = (snap.data() as dynamic)['following'];
+
+      if (following.contains(followId)) {
+        await _firestore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayRemove([uid]),
+        });
+
+        await _firestore.collection('users').doc(uid).update({
+          'followers': FieldValue.arrayRemove([followId]),
+        });
+      } else {
+        await _firestore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayUnion([uid]),
+        });
+
+        await _firestore.collection('users').doc(uid).update({
+          'followers': FieldValue.arrayUnion([followId]),
+        });
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
