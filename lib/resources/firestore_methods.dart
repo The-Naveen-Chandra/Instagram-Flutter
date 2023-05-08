3b485436b91aadca6416af6b890cf53bc9cf6eb8
Naveen Chandra <the.naveenchandra@gmail.com>
2023-05-08 17:17:10 +0530

    fix bug

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -109,30 +109,27 @@ class FirestoreMethods {
   }
 
   // follow user
-  Future<void> followUser(
-    String uid,
-    String followId,
-  ) async {
+  Future<void> followUser(String uid, String followId) async {
     try {
       DocumentSnapshot snap =
           await _firestore.collection('users').doc(uid).get();
-      List following = (snap.data() as dynamic)['following'];
+      List following = (snap.data()! as dynamic)['following'];
 
       if (following.contains(followId)) {
         await _firestore.collection('users').doc(followId).update({
-          'followers': FieldValue.arrayRemove([uid]),
+          'followers': FieldValue.arrayRemove([uid])
         });
 
         await _firestore.collection('users').doc(uid).update({
-          'followers': FieldValue.arrayRemove([followId]),
+          'following': FieldValue.arrayRemove([followId])
         });
       } else {
         await _firestore.collection('users').doc(followId).update({
-          'followers': FieldValue.arrayUnion([uid]),
+          'followers': FieldValue.arrayUnion([uid])
         });
 
         await _firestore.collection('users').doc(uid).update({
-          'followers': FieldValue.arrayUnion([followId]),
+          'following': FieldValue.arrayUnion([followId])
         });
       }
     } catch (e) {
